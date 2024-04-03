a77c051ac4dd455494e9fc61bc684967efea5692
paulabonifazi <paula.bonifazi.aquino@gmail.com>
2024-04-03 17:11:41 -0300

    .

--- MV/funciones.h
+++ MV/funciones.h
@@ -4,7 +4,8 @@ typedef struct TOperando{
     char parteReg; //la posicion se usa solo para registros y memoria y partereg solo para registros
     long int valor;
 } TOperando;
-/* podríamos usar este tipo para los operandos así almacenamos toda la info*/
+/* podríamos usar este tipo para los operandos así almacenamos toda la info
+    no sé si op1 y op2 sean parte de la MV o definirlas en el main*/
 
 void decodifica_cod_op(char *op1,char *op2,char *cod_op);
 
